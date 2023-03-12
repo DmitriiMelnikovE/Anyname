f6f4c839c2dbcdc8fc1a7049e0d57483ce10cbe3
Dmitrii Melnikov <d1990m@mail.ru>
2023-03-12 19:00:31 +0300

    complete day8 tasks

---
+++ src/main/java/day8/Task2.java
@@ -0,0 +1,9 @@
+package day8;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Airplane airplane = new Airplane("Plane", 2000, 50, 100);
+        airplane.info();
+        System.out.println(airplane.toString());
+    }
+}
